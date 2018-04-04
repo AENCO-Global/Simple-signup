d68ec7eead4ccc2402e37ee68219c77c1a423cad
Geoffrey Tipton <tipton@mysecretroom.com>
2018-04-04 16:00:54 +0800

    form delivery improvement

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-	// Safely catch the form paramters, only allow the form requested.
-	switch ($_GET['form']) {
+/* Loading page to load forms to catch the following
+	 -------------------------------
+	| signup.aencoin.com?form=kyc   |
+	| signup.aencoin.com?form=kyb   |
+	| signup.aencoin.com?form=login |
+	 -------------------------------
+*/
+// Safely catch the form paramters, only allow the form requested.
+	if (isset($_REQUEST['form'])) 
+		$form = $_REQUEST['form'];
+	else
+		$form = "";
+	endif
+
+	switch ($form) {
 
 	    case "kyc":
 	    	$template = 'forms/kyc.html';
@@ -10,15 +23,155 @@
 			$template = 'forms/kyb.html';
 	        break;
 
-	    default: 
-	        // value is not on the list. React accordingly.
-	        print "Invalid form Request";
-	        end.
+	    default:
+	        // default form
+	    	$template = 'forms/kyc.html';
+	    	break;
 	}
 
 	include_once('api/view.php');
 	$form = new View();
 	$form->render($template);
 
-	phpInfo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// What are you doing dow here you IDIOT?
 ?>
