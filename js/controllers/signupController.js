924d567c223428c266fb9fcf302eb80f57036505
David Carey <david@badbod.com>
2018-04-05 08:43:22 +0700

    added dev version

--- js/controllers/signupController.js
+++ js/controllers/signupController.js
@@ -1,25 +1,19 @@
 signup_aencoin.controller( 'signupController' , function( $scope, $timeout, $http ){
 
-    var email = checkCookie('email');
-    var key = checkCookie('key');
 
-    if( email && key ){
-        $http.post('api/auth.php' , { email: getCookie('email'), key: getCookie('key') } )
-            .success( function(data){
-                if( data.success ){
-                    $scope.data.authorised = true;
-                    $scope.data.customer_name = data.customer_name;
-                    window.location.assign('#/orders');
-                }
-            })
-            .error( function(data){
-                alert('unknown error talking to server. Please try again later.');
-            });
-    }else{
-        $scope.showForm = 'signin';
-    }
 
-    $scope.doLogin = function(){
+	$http.get('version.html')
+		.then(function(data){
+			console.log(data.data);
+			$scope.version = data.data;
+		})
+		, function(data){
+			conmsole.log(data);
+		};
+
+	$scope.showForm = 'signin';
+    
+	$scope.doLogin = function(){
         $http.post( 'api/auth.php' , $scope.formdata )
             .success( function(data){
                 var cdays = false;
@@ -32,24 +26,6 @@ signup_aencoin.controller( 'signupController' , function( $scope, $timeout, $htt
                     $scope.data.authorised = true;
                     $scope.data.customer_name = data.customer_name;
                     window.location.assign('#/orders');
-                }else{
-                    $scope.formError = data.message;
-                    $scope.data.authorised = false;
-                    $timeout( function(){
-                        $scope.formError = '';
-                    },5000);
-                }
-            })
-            .error( function(data){
-                alert('Unknown error talking to server. Please try again later.');
-            });
-    };
-
-    $scope.sendPass = function(){
-        $http.post( 'api/send-pass.php' , $scope.formdata )
-            .success( function(data){
-                if( data.success ){
-                    $scope.showForm = 'sent';
                 }else{
                     $scope.formError = data.message;
                     $scope.data.authorised = false;
