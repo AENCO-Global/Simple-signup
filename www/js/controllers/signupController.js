3be9f8e00f9154d4099d6e82bfb5e948a2cd0047
David Carey <david@badbod.com>
2018-04-06 15:56:10 +0700

    initial db save

--- www/js/controllers/signupController.js
+++ www/js/controllers/signupController.js
@@ -13,29 +13,15 @@ signup_aencoin.controller( 'signupController' , function( $scope, $timeout, $htt
 
 	$scope.showForm = 'signin';
     
-	$scope.doLogin = function(){
-        $http.post( 'api/auth.php' , $scope.formdata )
+	$scope.doSignup = function(){
+        $http.post( 'api/kyc.php' , $scope.formdata )
             .success( function(data){
-                var cdays = false;
                 if( data.success ){
-                    if( $scope.formdata.remember ){
-                        cdays = 1000;
-                    }
-                    setCookie('email', $scope.formdata.email, cdays );
-                    setCookie('key', data.key, cdays );
-                    $scope.data.authorised = true;
-                    $scope.data.customer_name = data.customer_name;
-                    window.location.assign('#/orders');
-                }else{
-                    $scope.formError = data.message;
-                    $scope.data.authorised = false;
-                    $timeout( function(){
-                        $scope.formError = '';
-                    },5000);
+                    
                 }
             })
             .error( function(data){
-                alert('Unknown error talking to server. Please try again later.');
+                //alert('Unknown error talking to server. Please try again later.');
             });
     };
 });
